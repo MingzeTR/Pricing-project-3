fix analytic formula bugs

simulation_code.py
@@ -33,21 +33,21 @@ def risk_neutral_int_elur(r0, alpha, beta, sigma, theta0, phi, eta, t, w_sim_r, 
 
 
 def analytic_a(alpha, beta, sigma, phi, eta, T, t):
-    m = alpha * (T - t)
-    n = beta * (T - t)
+    m = e ** -(alpha * (T - t))
+    n = e ** -(beta * (T - t))
     p1 = - phi * (T - t)
     p2 = - phi * beta * (1 - m) / (alpha * (alpha - beta))
     p3 = phi * alpha * (1 - n) / (beta * (alpha - beta))
     pb = sigma ** 2 / (2 * alpha ** 2) * (T - t) \
         - sigma ** 2 * (1- m) / alpha ** 3 \
-        + (1 - m ** 2) / (4 * alpha ** 3)
+        + sigma ** 2 * (1 - m ** 2) / (4 * alpha ** 3)
     pc = eta ** 2 / (2 * beta ** 2) * (
         (T - t)
         + 2 * beta * (1 - m) / (alpha * (alpha - beta))
         - 2 * alpha * (1 - n) / (beta * (alpha - beta))
         + beta ** 2 * (1 - m ** 2) / (2 * alpha * (alpha - beta) ** 2)
         - 2 * alpha * beta * (1 - m * n) / ((alpha + beta) * (alpha - beta) ** 2)
-        + alpha ** 2 *(1 - n ** 2) / (2 * beta * (alpha - beta) ** 2)
+        + alpha ** 2 * (1 - n ** 2) / (2 * beta * (alpha - beta) ** 2)
     )
 
     return p1 + p2 + p3 + pb + pc
@@ -58,12 +58,12 @@ def analytic_b(alpha, T, t):
 
 
 def analytic_c(alpha, beta, T, t):
-    return ((e ** -(alpha * (T - t)) - 1) / (alpha - beta)
-            + alpha * (1 - e ** -(beta * (T - t))) / ((alpha - beta) * beta))
+    return (1 / beta
+            + (beta * e ** -(alpha * (T - t)) - alpha * e ** -(beta * (T - t))) / (beta * (alpha - beta)))
 
 
 def analytic_formula_curve(r0, alpha, beta, sigma, theta0, phi, eta, T, t):
-    return (analytic_c(alpha, beta, T, t) * r0
+    return (analytic_b(alpha, T, t) * r0
             + analytic_c(alpha, beta, T, t) * theta0
             - analytic_a(alpha, beta, sigma, phi, eta, T, t)) / (T - t)
 
@@ -86,19 +86,20 @@ if __name__ == '__main__':
     theta_path_set = np.zeros((nsims, nsteps))
     r_path_set = np.zeros((nsims, nsteps))
 
-    for i in range(0, nsims):
-        w_sim_r = Sim_Brownian_Motion(t)
-        w_sim_theta = Sim_Brownian_Motion(t)
+    # for i in range(0, nsims):
+    #     w_sim_r = Sim_Brownian_Motion(t)
+    #     w_sim_theta = Sim_Brownian_Motion(t)
+    #
+    #     [theta_path_set[i:], r_path_set[i:]] = risk_neutral_int_elur(r0, alpha, beta, sigma, theta0, phi, eta, t, w_sim_r, w_sim_theta)
 
-        [theta_path_set[i:], r_path_set[i:]] = risk_neutral_int_elur(r0, alpha, beta, sigma, theta0, phi, eta, t, w_sim_r, w_sim_theta)
-
-    mc_bond_yield = int_matrix = np.matrix(r_path_set.reshape(1000,100)).mean(0)
+    # mc_bond_yield = int_matrix = np.matrix(r_path_set.reshape(1000,100)).mean(0)
 
     analytic_bond_yield = np.zeros(nsteps)
+    analytic_bond_yield[0] = r0
     for i in range(1, nsteps):
         analytic_bond_yield[i] = analytic_formula_curve(r0, alpha, beta, sigma, theta0, phi, eta, t[i], t=0)
 
-    plt.plot(t, np.asarray(mc_bond_yield).reshape(-1), label='MC bond yield')
+    # plt.plot(t, np.asarray(mc_bond_yield).reshape(-1), label='MC bond yield')
     plt.plot(t, analytic_bond_yield, label='Analytic bond yield')
     plt.xlabel('Time')
     plt.ylabel('Yield')
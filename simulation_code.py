feb2267b087a85f6357028e8eecb801c920289ec
MingzeTR <cmzjs0731@gmail.com>
2021-11-28 11:48:03 -0500

    improve graph

--- simulation_code.py
+++ simulation_code.py
@@ -34,15 +34,16 @@ def risk_neutral_int_elur(r0, alpha, beta, sigma, theta0, phi, eta, t, w_sim_r, 
 
 def bond_price_sim(r_path, t):
     dt = t[1] - t[0]
-    bank_account = np.zeros(len(t))
+    bank_account = np.zeros(len(t)+1)
     bank_account[0] = 1
-    for i in range(0, len(t) - 1):
+    for i in range(0, len(t)):
         bank_account[i+1] = bank_account[i] + r_path[i] * bank_account[i] * dt
     bond_price = [1 / b for b in bank_account]
-    return bond_price
+    return bond_price[1:]
 
 
 def bond_yield_sim(int_matrix, nsims, nsteps, t):
+    dt = t[1] - t[0]
     # interest rate to price
     mc_bond_price = np.empty([nsims,nsteps])
     for n in range(0, nsims):
@@ -61,16 +62,18 @@ def bond_yield_sim(int_matrix, nsims, nsteps, t):
 
 
     mc_bond_yield = np.zeros(len(t))
-    mc_bond_yield[0] = r0
+    # mc_bond_yield[0] = r0
     mc_bond_yield_upper = np.zeros(len(t))
-    mc_bond_yield_upper[0] = r0
+    # mc_bond_yield_upper[0] = r0
     mc_bond_yield_lower = np.zeros(len(t))
-    mc_bond_yield_lower[0] = r0
+    # mc_bond_yield_lower[0] = r0
 
-    for i in range(1, nsteps):
-        mc_bond_yield[i] = - np.log(mc_bond_price_avg[i]) / t[i]
-        mc_bond_yield_upper[i] = - np.log(mc_bond_price_upper[i]) / t[i]
-        mc_bond_yield_lower[i] = - np.log(mc_bond_price_lower[i]) / t[i]
+    for i in range(0, nsteps):
+        mc_bond_yield[i] = - np.log(mc_bond_price_avg[i]) / (dt * (i + 1))
+        mc_bond_yield_upper[i] = - np.log(mc_bond_price_upper[i]) / (dt * (i + 1))
+        mc_bond_yield_lower[i] = - np.log(mc_bond_price_lower[i]) / (dt * (i + 1))
+    mc_bond_yield_lower = mc_bond_yield - mc_bond_yield_lower
+    mc_bond_yield_upper = mc_bond_yield_upper - mc_bond_yield
     return mc_bond_yield, mc_bond_yield_upper, mc_bond_yield_lower
 
 
@@ -113,7 +116,7 @@ def analytic_formula_curve(r0, alpha, beta, sigma, theta0, phi, eta, T, t):
 
 if __name__ == '__main__':
     T = 10
-    nsteps = 100
+    nsteps = 40
     nsims = 1000
     r0 = 0.02
     alpha = 3
@@ -134,7 +137,7 @@ if __name__ == '__main__':
         w_sim_r = Sim_Brownian_Motion(t)
         w_sim_theta = Sim_Brownian_Motion(t)
         [theta_path_set[i:], r_path_set[i:]] = risk_neutral_int_elur(r0, alpha, beta, sigma, theta0, phi, eta, t, w_sim_r, w_sim_theta)
-    int_matrix = r_path_set.reshape(1000, 100)
+    int_matrix = r_path_set.reshape(nsims, nsteps)
     # r_path_set_avg = np.asarray(r_path_set_avg).reshape(-1)
 
     # simulate yield curve
@@ -146,13 +149,28 @@ if __name__ == '__main__':
     for i in range(1, nsteps):
         analytic_bond_yield[i] = analytic_formula_curve(r0, alpha, beta, sigma, theta0, phi, eta, t[i], t=0)
 
-    plt.plot(t, mc_bond_yield, label='MC bond yield')
-    plt.plot(t, mc_bond_yield_upper, label='MC bond yield upper')
-    plt.plot(t, mc_bond_yield_lower, label='MC bond yield lower')
-
+    # analytic vs mc
+    plt.figure(1)
+    plt.errorbar(t, mc_bond_yield, yerr=[mc_bond_yield_lower,mc_bond_yield_upper], fmt='-', elinewidth=0.5, capsize=2, label='MC bond yield with error band')
     plt.plot(t, analytic_bond_yield, label='Analytic bond yield')
     plt.xlabel('Time')
     plt.ylabel('Yield')
+    plt.title('Analytic vs Monte Carlo Simulation  Bond Yield Comparison')
     plt.legend()
-    plt.savefig('A3_Q2.jpg')
+    plt.savefig('A3_Q2_MCvsAna.jpg')
+
+    # mean reverting graphs
+    w_sim_r_for_graph = Sim_Brownian_Motion(np.linspace(0, T, nsims))
+    w_sim_theta_for_graph = Sim_Brownian_Motion(np.linspace(0, T, nsims))
+    [theta_path_set_for_graph, r_path_set_for_graph] = risk_neutral_int_elur(r0, alpha, beta, sigma, theta0, phi, eta, np.linspace(0, T, nsims), w_sim_r_for_graph,
+                                                                 w_sim_theta_for_graph)
+    plt.figure(2)
+    plt.plot(np.linspace(0, T, nsims), theta_path_set_for_graph, label='Long Run Interest Rate')
+    plt.plot(np.linspace(0, T, nsims), r_path_set_for_graph, label='Short Run Interest Rate')
+    plt.plot(np.linspace(0, T, nsims), np.ones(nsims)*phi, label='Mean Reverting Level (\u03C6)')
+    plt.xlabel('Time')
+    plt.ylabel('Rate')
+    plt.title('Stochastic Interest Rate Simulation')
+    plt.legend()
+    plt.savefig('A3_Q2_MeanRevert.jpg')
 
